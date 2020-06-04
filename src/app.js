b685ccbacdfcc31ddc7229ebc691c3dc93b6869f
Edy Ruíz <edyrui2177@gmail.com>
2020-06-04 21:40:08 +0200

    make it better

--- src/app.js
+++ src/app.js
@@ -1,20 +1,16 @@
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const routes = require("./routes/index.routes");
 require("dotenv").config();
 require("./database");
 
 // Settings
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.set("port", process.env.PORT || 3000);
 app.use("/api", routes);
 app.use(morgan("dev"));
-
-// Routes
-app.use(require("./routes/index.routes"));
 
 // Start server
 app.listen(app.get("port"), () => {
