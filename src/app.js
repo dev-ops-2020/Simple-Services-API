f0fbef64d445ea47b717ebc36b82e29b003a3850
Edy Ruíz <edyrui2177@gmail.com>
2020-06-04 21:30:38 +0200

    make it better

--- src/app.js
+++ src/app.js
@@ -1,26 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser')
-const morgan = require('morgan');
-const routes = require('./routes/index.routes');
-require('dotenv').config();
-require('./database');
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const routes = require("./routes/index.routes");
+require("dotenv").config();
+require("./database");
 
 // Settings
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.set('port', process.env.PORT || 3000)
-app.use('/api', routes)
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.set("port", process.env.PORT || 3000);
+app.use("/api", routes);
+app.use(morgan("dev"));
 
 // Routes
-app.use(require('./routes/index.routes'));
-
-// Middlewares
-app.use(morgan('dev'));
-app.use(express.urlencoded({extended:false}));
-app.use(express.json());
+app.use(require("./routes/index.routes"));
 
 // Start server
-app.listen(app.get('port'), () => {
-    console.log('Server on port ', app.get('port'));
-})
+app.listen(app.get("port"), () => {
+  console.log("Server on port ", app.get("port"));
+});
