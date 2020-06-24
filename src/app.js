a5834f780d9810cde22a742fb51f635e5de3fc05
Edy Ruíz <edyrui2177@gmail.com>
2020-06-24 00:23:00 -0600

    make ir better

--- src/app.js
+++ src/app.js
@@ -1,24 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const multer = require('multer');
-const path = require('path');
 const app = express();
 require('dotenv').config();
 require('./database');
 
-const storage = multer.diskStorage({
-  filename: (req, file, cb) => {
-    cb(null, );
-  },
-  destination: path.join(__dirname, 'public/uploads'),
-  filename: (req, file, cb) => {
-    cb(null, new Date().getTime() + path.extname(file.originalname));
-  }
-});
 
 app.use(morgan('dev'));
-app.use(multer({storage}).single('image'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api', require('./routes/index.routes'));
