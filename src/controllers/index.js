fee438083e13775d1d34a7cd63b51fab2ccafd7d
Edy Ruíz <edyrui2177@gmail.com>
2020-06-30 15:21:10 -0600

    make it better

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -11,7 +11,12 @@ function Index(req, res) {
 function Terms(req, res) {
   const response = {
     header: 'Términos y condiciones de Simple Services',
-    message: 'Acá van a ir los Términos y condiciones, los pondré cuando Carlitus los haga :v'
+    message: 'Acá van a ir los Términos y condiciones, los pondré cuando Carlitus los haga :v\n\n' + 
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque tenetur saepe repellat, fugiat perferendis temporibus quisquam mollitia optio blanditiis unde\n'+
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque nisi dolor at beatae, impedit, quis odit vel id omnis laborum repudiandae asperiores harum quae nobis vero tempore recusandae mollitia earum dolores? Sequi est quod at, earum nemo quaerat doloremque optio, explicabo cupiditate unde, voluptatibus voluptas numquam aperiam repudiandae quibusdam alias\n'+    
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt omnis nihil id facere nemo, molestias consequuntur delectus modi unde eligendi non error quam voluptatem accusamus commodi nam repellat totam. Eius!\n'+
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, aliquam quasi, autem laudantium maxime voluptatum, impedit labore maiores aperiam fuga nihil quia inventore omnis provident quis! Doloremque quo necessitatibus odio.\n'+
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores, odit.'
   };
   res.send(response);
 }
