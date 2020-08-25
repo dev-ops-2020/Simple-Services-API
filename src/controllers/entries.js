f7b5c5a1d29b778de00993843bbce70757031e7b
Edy Ruíz <edyrui2177@gmail.com>
2020-08-25 12:15:44 -0600

    make it better

--- src/controllers/entries.js
+++ src/controllers/entries.js
@@ -4,9 +4,8 @@ const moment = require('moment');
 function CreateEntry(req, res) {
   moment.locale('es');
   const date = moment().format('L');
-  moment.locale('es-us');
-  const time = moment().format('LT');
-  const current = date + " " + time;
+  const time = moment().format('LTS');
+  const current = date + "-" + time;
   let Entry = new EntriesSchema();
   Entry.image = req.body.image;
   Entry.desc = req.body.desc;
@@ -36,12 +35,12 @@ function ReadEntry(req, res) {
 
 function ListEntries(req, res) {
   EntriesSchema.find({status: true}, (err, Entries) => {
-    if (Entries.length == 0 && !Entries.status) {
+    if (Entries.length == 0) {
       return res.status(202).send({message: 'No entries to show'});
     } else {
       return res.status(200).send({message: 'Ok', entries: Entries});
     }
-  }).sort({date: 1});
+  });
 }
 
 function ListEntriesByBusiness(req, res) {
