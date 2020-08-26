ecf72a90445de7cd82f8cd919c1bb148b5389c05
Edy Ruíz <edyrui2177@gmail.com>
2020-08-26 06:11:01 -0600

    make it better

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const favicon = require('serve-favicon');
+const path = require('path');
 const app = express();
 require('dotenv').config();
 require('./database');
 
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
